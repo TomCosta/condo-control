1794a71627b98a765927f606dab42a773bc14e42
Wagner D'Amaral <32019817+wdamaral@users.noreply.github.com>
2017-11-17 13:49:39 -0500

    Loading feature -> Spaces

--- src/app/control-panel/spaces/spaces-list/space-item/space-item.component.ts
+++ src/app/control-panel/spaces/spaces-list/space-item/space-item.component.ts
@@ -2,11 +2,8 @@ import { Component, Input } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Store } from '@ngrx/store';
 import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
-// import { Subscription } from 'rxjs/Subscription';
-// import 'rxjs/add/operator/take';
 
 import { Space } from '../../../shared/space.model';
-// import { SpacesService } from '../../spaces.service';
 import * as SpacesActions from '../../store/spaces.actions';
 import * as fromSpaces from '../../store/spaces.reducers';
 
@@ -22,15 +19,11 @@ export class SpaceItemComponent {
   private editMode = false;
   private modalReference: any;
   private enableSB = false;
-  // updatedSpaceName: string;
-  // updatedSpace: Space;
-  // private subscription: Subscription;
   updatedSpaceName: string;
   updatedPicture: string;
   closeResult: string;
 
   constructor(private modalService: NgbModal,
-              // private spacesService: SpacesService,
               private store: Store<fromSpaces.FeatureState>) { }
 
   onEnableEdit() {
@@ -38,13 +31,11 @@ export class SpaceItemComponent {
 
     this.updatedSpaceName = this.space.name;
     this.updatedPicture = this.space.picture;
-    // console.log(this.updatedSpace);
 
   }
   onUpdate() {
     this.onCancelEdit();
     const updatedSpace = new Space(this.updatedSpaceName, this.updatedPicture);
-    // this.spacesService.updateSpace(updatedSpace, this.index);
     this.store.dispatch(new SpacesActions.UpdateSpaceRequest({$key: this.space.$key, updatedSpace: updatedSpace}));
   }
 
@@ -54,12 +45,10 @@ export class SpaceItemComponent {
   }
 
   onCancelAdd() {
-    // this.spacesService.addModeActivated.next(false);
     this.store.dispatch(new SpacesActions.SwitchAddMode(false));
   }
 
   onDelete() {
-    // this.spacesService.deleteSpace(this.index);
     this.store.dispatch(new SpacesActions.DeleteSpaceRequest(this.space.$key));
   }
 
@@ -73,16 +62,10 @@ export class SpaceItemComponent {
   }
 
   open(content) {
-    // this.showSnackBar();
     this.modalReference = this.modalService.open(content);
-      // this.closeResult = `Closed with: ${result}`;
-    // }, (reason) => {
-    //   this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
-    // });
   }
 
   onConfirmExclusion () {
-    // this.showSnackBar();
     this.modalReference.close();
     this.onDelete();
 
